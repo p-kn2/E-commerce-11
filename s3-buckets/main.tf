997bddb57e29d4fe22cccfbeb127bb4379972294
Piyush <piyush@Piyushs-MacBook-Air.local>
2026-04-08 16:15:39 +0530

    s3 changes

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "pk1-bucket1"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "pk1-bucket1"
     Environment = "dev"
   }
 }
@@ -19,16 +19,34 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "pk1-bucket2"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "pk1-bucket2"
     Environment = "dev"
   }
 }
 
 resource "aws_s3_bucket_versioning" "bucket2_versioning" {
   bucket = aws_s3_bucket.bucket2.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+
+resource "aws_s3_bucket" "bucket3" {
+  bucket = "pk1-bucket3"
+
+  tags = {
+    Name        = "pk1-bucket3"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bucket3_versioning" {
+  bucket = aws_s3_bucket.bucket3.id
   versioning_configuration {
     status = "Enabled"
   }
--- s3-buckets/variables.tf
+++ s3-buckets/variables.tf
@@ -1,13 +1,19 @@
 variable "bucket1_name" {
   description = "Name of the first S3 bucket"
   type        = string
-  default     = "aluruarumullaa1"
+  default     = "pk1-bucket1"
 }
 
 variable "bucket2_name" {
   description = "Name of the second S3 bucket"
   type        = string
-  default     = "arumullaaluruu1"
+  default     = "pk1-bucket2"
+}
+
+variable "bucket3_name" {
+  description = "Name of the third S3 bucket"
+  type        = string
+  default     = "pk1-bucket3"
 }
 
 variable "environment" {
